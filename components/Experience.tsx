fc2ae9727ff87078ac70ee905f622a36e2245be5
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-05-03 14:53:02 +0200

    Responsive fix- Experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,35 +1,35 @@
 export function Experience() {
 
   return (
-    <div className="h-[100dvh] w-full max-w-7xl mx-auto">
-      <h2 className="text-[3rem] font-bold tracking-[0.125rem] text-center my-10 2xl:text-[4rem]">
+    <div className="lg:h-[100dvh] w-full max-w-7xl mx-auto">
+      <h2 className="my-10 h-[10dvh] font-bold tracking-[0.125rem] text-center text-[1.75rem] lg:text-[3rem] 2xl:text-[4rem] px-8">
           my journey at Temenos.
       </h2>
       <div className="w-full flex max-sm:flex-col items-start justify-center gap-4 px-4">
-        <div className="flex-1 flex flex-col">
-          <h2 className="text-xl font-bold my-4 2xl:text-[1.75rem]">T24 Software Developer</h2>
-          <p className="pb-4 2xl:text-[1.25rem]">December 2016- Decemeber 2019</p>
-          <ul className="flex flex-col gap-1.5 2xl:text-[1.5rem]">
+        <div className="flex-1 flex flex-col gap-2">
+          <h2 className="text-[1.125rem] lg:text-[1.25rem] font-bold 2xl:text-[1.5rem]">T24 Software Developer</h2>
+          <p className="pb-4 max-lg:text-[0.9rem] italic">December 2016- Decemeber 2019</p>
+          <ul className="flex flex-col gap-1.5 max-lg:text-[0.9rem]">
             <li>Developed and tested software solutions based on requirements and technical documents provided.</li>
             <li>Participated in continuous learning to improve coding techniques and ensure high-quality software delivery.</li>
             <li>Supported senior developers during critical project phases, contributing to successful project completions.</li>
           </ul>
         </div>
-        <div className="flex-1 flex flex-col">
-          <h2 className="text-xl font-bold my-4 2xl:text-[1.75rem]">T24 Software Developer</h2>
-          <p className="pb-4 2xl:text-[1.25rem]">January 2020- July 2021</p>
-          <ul className="flex flex-col gap-1.5 2xl:text-[1.5rem]">
+        <div className="flex-1 flex flex-col gap-2">
+          <h2 className="text-[1.125rem] lg:text-[1.25rem] font-bold 2xl:text-[1.5rem] pb-1 ">T24 Software Developer</h2>
+          <p className="pb-4 max-lg:text-[0.9rem] italic">January 2020- July 2021</p>
+          <ul className="flex flex-col gap-1.5 max-lg:text-[0.9rem]">
             <li>Analyzed requirements, prepared technical design documents, and contributed to solution development.</li>
             <li>Led product testing phases, ensuring software met all client requirements before final delivery.</li>
             <li>Collaborated with cross-functional teams to ensure projects were completed on time and to specifications.</li>
           </ul>
         </div>
       </div>
-      <div className="w-full flex max-sm:flex-col items-start justify-center gap-4 px-4">
-        <div className="flex-1 flex flex-col">
-          <h2 className="text-xl font-bold my-4 2xl:text-[1.75rem]">T24 Specialist</h2>
-          <p className="pb-4 2xl:text-[1.25rem]">August 2021- June 2022</p>
-          <ul className="flex flex-col gap-1.5 2xl:text-[1.5rem]">
+      <div className="w-full flex max-sm:flex-col items-start justify-center gap-4 px-4 mt-4">
+        <div className="flex-1 flex flex-col gap-2">
+          <h2 className="text-[1.125rem] lg:text-[1.25rem] font-bold 2xl:text-[1.5rem] pb-1 ">T24 Specialist</h2>
+          <p className="pb-4 max-lg:text-[0.9rem] italic">August 2021- June 2022</p>
+          <ul className="flex flex-col gap-1.5 max-lg:text-[0.9rem]">
             <li>Provided customer support and participated in requirement-gathering meetings, ensuring client needs were clearly understood and documented.</li>
             <li>Delivered client demos, showcasing the software&apos;s capabilities and gathering feedback for further refinement.</li>
             <li>Developed solutions, prepared technical design documents, and supported testing and post-rollout activities.</li>
@@ -37,10 +37,10 @@ export function Experience() {
             <li>Actively participated in analysis and delivery, contributing to a smooth development process and successful project outcomes.</li>
           </ul>
         </div>
-        <div className="flex-1 flex flex-col">
-          <h2 className="text-xl font-bold my-4 2xl:text-[1.75rem]">T24 Associate Lead Product Engineer</h2>
-          <p className="pb-4 2xl:text-[1.25rem]">July 2022- December 2022</p>
-          <ul className="flex flex-col gap-1.5 2xl:text-[1.5rem]">
+        <div className="flex-1 flex flex-col gap-2">
+          <h2 className="text-[1.125rem] lg:text-[1.25rem] font-bold 2xl:text-[1.5rem] pb-1 ">T24 Associate Lead Product Engineer</h2>
+          <p className="pb-4 max-lg:text-[0.9rem] italic">July 2022- December 2022</p>
+          <ul className="flex flex-col gap-1.5 max-lg:text-[0.9rem]">
             <li>In addition to the responsibilities held as a T24 Specialist, I led technical initiatives, coordinating with stakeholders to ensure timely delivery and alignment with client expectations.</li>
             <li>Conducted risk assessments and developed mitigation strategies to address potential challenges during project execution.</li>
           </ul>
