d1e257c81ed57acfb69dadba30f6ba6f2c522d37
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-05-03 14:04:59 +0200

    Responsive fix- About section

--- components/About.tsx
+++ components/About.tsx
@@ -1,16 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "motion/react";
 import Image from "next/image";
 import { SiClerk, SiCss3, SiFirebase, SiGit, SiJavascript, SiMongodb, SiNextdotjs, SiOpenai, SiReact, SiReactrouter, SiShadcnui, SiTailwindcss, SiTypescript, SiZod } from "react-icons/si";
 import GsapLogo from "./GsapLogo";
+import { cn } from "@/lib/utils";
+import { IconArrowNarrowLeft, IconArrowNarrowRight } from "@tabler/icons-react";
 
 export function BentoGridThirdDemo() {
   return (
-    <section id="about" className="h-[100dvh] w-full flex flex-col max-w-7xl mx-auto">
-      <h1 className="text-[3rem] font-bold tracking-[0.125rem] lowercase text-center 2xl:text-[4rem]">about me.</h1>
-      <div className="flex-1 py-8 flex max-md:flex-col items-center justify-center tracking-wide gap-2">
-        <div className="flex-1 self-start px-4 text-[1rem] 2xl:text-[1.5rem]">
+    <section id="about" className="w-full flex flex-col max-w-7xl mx-auto">
+      <h2 className="my-10 h-[10dvh] font-bold tracking-[0.125rem] text-center text-[1.75rem] lg:text-[3rem] 2xl:text-[4rem]">about me.</h2>
+      <div className="flex-1 w-full lg:py-8 flex flex-col items-center justify-center tracking-wide gap-2">
+        <div className="flex-1 self-start px-4 max-lg:text-[0.9rem]">
           <ProfilePicture />
           I&apos;m a passionate self-taught front-end developer with a background in banking software (T24). After nearly six years in the fintech industry, I transitioned into web development to bring ideas to life visually and interactively.<br /><br />
 
@@ -24,72 +26,72 @@ export function BentoGridThirdDemo() {
 
           For more details on my background, education, and work experience, feel free to download my <a href="/Krishnapriya_Venkatakrishnan_Resume.pdf" download className="underline">resume</a>.
         </div>
-        <div className="w-[40%] self-start flex flex-col gap-4">
-          <div>
-            <p className="font-bold pb-1 border-b-1 max-w-[450px] 2xl:text-[1.5rem] 2xl:max-w-[550px]">Technologies</p>
+        <div className="w-full self-start flex max-lg:flex-col justify-between gap-4 px-4 lg:mt-6">
+          <div className="flex-1">
+            <p className="text-[1.125rem] lg:text-[1.25rem] font-bold 2xl:text-[1.5rem] pb-1 ">technologies.</p>
             <table className="flex">
               <thead></thead>
-              <tbody>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+              <tbody className="mt-4">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">Frontend</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiReact className="text-[#61DAFB] text-2xl" />
-                      <SiNextdotjs className="text-[#000] dark:text-[#fff] text-2xl" />
-                      <SiTypescript className="text-[#3178C6] text-2xl" />
-                      <SiJavascript className="text-[#F7DF1E] text-2xl" />
-                      <SiTailwindcss className="text-[#06B6D4] text-2xl" />
-                      <SiCss3 className="text-[#264DE4] text-2xl" />
+                      <SiReact className="text-[#61DAFB] lg:text-2xl" />
+                      <SiNextdotjs className="text-[#000] dark:text-[#fff] lg:text-2xl" />
+                      <SiTypescript className="text-[#3178C6] lg:text-2xl" />
+                      <SiJavascript className="text-[#F7DF1E] lg:text-2xl" />
+                      <SiTailwindcss className="text-[#06B6D4] lg:text-2xl" />
+                      <SiCss3 className="text-[#264DE4] lg:text-2xl" />
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">Backend & Databases</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiMongodb className="text-green-400 text-2xl" />
-                      <SiFirebase className="text-[#FFCA28] text-2xl"/>
-                      <Image src="/assets/next-auth.png" alt="next-auth" height={25} width={25} className="rounded-full max-sm:h-[20px] max-sm:w-[20px]" />
-                      <Image src="/assets/logo-live-docs.png" alt="liveblocks" height={25} width={25} className="rounded-full max-sm:h-[20px] max-sm:w-[20px]" />
+                      <SiMongodb className="text-green-400 lg:text-2xl" />
+                      <SiFirebase className="text-[#FFCA28] lg:text-2xl"/>
+                      <Image src="/assets/next-auth.png" alt="next-auth" height={25} width={25} className="rounded-full max-lg:h-[20px] max-lg:w-[20px]" />
+                      <Image src="/assets/logo-live-docs.png" alt="liveblocks" height={25} width={25} className="rounded-full max-lg:h-[20px] max-lg:w-[20px]" />
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">UI & Design Systems</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiShadcnui className="text-[#000] dark:text-[#fff] text-xl" />
-                      <span className="font-sans font-black  dark:text-white">Aceternity UI</span>
+                      <SiShadcnui className="text-[#000] dark:text-[#fff] lg:text-xl" />
+                      <span className="font-sans font-black  dark:text-white text-[1.125rem]">Aceternity UI</span>
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">Authentication</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiClerk className="text-[#000] dark:text-[#fff] text-2xl" />
-                      <Image src="/assets/next-auth.png" alt="next-auth" height={25} width={25} className="rounded-full max-sm:h-[20px] max-sm:w-[20px]" />
+                      <SiClerk className="text-[#000] dark:text-[#fff] lg:text-2xl" />
+                      <Image src="/assets/next-auth.png" alt="next-auth" height={25} width={25} className="rounded-full max-lg:h-[20px] max-lg:w-[20px]" />
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">Validation</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiZod className="text-[#06B6D4] text-2xl" />
+                      <SiZod className="text-[#06B6D4] lg:text-2xl" />
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">Routing</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiReactrouter className="text-[#CA4245] text-2xl" />
-                      <SiNextdotjs className="text-[#000] dark:text-[#fff] text-2xl" />
+                      <SiReactrouter className="text-[#CA4245] lg:text-2xl" />
+                      <SiNextdotjs className="text-[#000] dark:text-[#fff] lg:text-2xl" />
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">Animations</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
@@ -97,24 +99,25 @@ export function BentoGridThirdDemo() {
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b-1 2xl:text-[1.5rem]">
+                <tr className="border-b-1 max-lg:text-[0.9rem]">
                   <td className="py-2">APIs & Tools</td>
                   <td className="px-2">
                     <div className="flex items-center gap-4">
-                      <SiOpenai className="text-2xl"/>
-                      <SiGit className="text-[#F05032] text-2xl" />
-                      <Image src="/assets/vs-code-logo.svg" alt="vs-code" height={25} width={25} className="rounded-full max-sm:h-[20px] max-sm:w-[20px]" />
+                      <SiOpenai className="lg:text-2xl"/>
+                      <SiGit className="text-[#F05032] lg:text-2xl" />
+                      <Image src="/assets/vs-code-logo.svg" alt="vs-code" height={25} width={25} className="rounded-full max-lg:h-[20px] max-lg:w-[20px]" />
                     </div>
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
-          <div className="flex flex-col gap-4 justify-start">
-            <p className="max-w-[450px] font-bold py-1 border-b-1 2xl:text-[1.5rem] 2xl:max-w-[550px]">Certificates</p>
+          <div className="flex flex-1 flex-col gap-4 justify-start ">
+            <p className="text-[1.125rem] lg:text-[1.25rem] font-bold 2xl:text-[1.5rem] pb-1 ">certificates.</p>
             <Certificates />
           </div>
         </div>
+        
       </div>
     </section>
   );
@@ -139,20 +142,20 @@ const ProfilePicture = () => {
         repeat: Infinity,
         repeatType: "reverse",
       }}
-      className="flex flex-1 w-[255px] h-[255px] min-h-[6rem] dark:bg-dot-white/[0.2] rounded-full bg-dot-black/[0.2] flex-col float-left mr-4"
+      className="flex flex-1 max-lg:w-[175px] max-lg:h-[175px] w-[255px] h-[255px] min-h-[6rem] dark:bg-dot-white/[0.2] rounded-3xl bg-dot-black/[0.2] flex-col float-left mr-4"
       style={{
         background:
           "linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)",
         backgroundSize: "400% 400%",
       }}
     >
-      <motion.div className="relative h-full w-full rounded-full flex items-center justify-center">
+      <motion.div className="relative h-full w-full rounded-3xl flex items-center justify-center">
         <Image
         src="/assets/profile.jpg"
         alt="Profile"
         width={250}
         height={250}
-        className="rounded-full border-2"
+        className="rounded-3xl border-4"
         />
       </motion.div>
     </motion.div>
@@ -160,58 +163,20 @@ const ProfilePicture = () => {
 };
 
 const Certificates = () => {
-  const first = {
-    initial: {
-      x: 20,
-      rotate: 0,
-    },
-    hover: {
-      x: 0,
-      rotate: 0,
-    },
-  };
-  const second = {
-    initial: {
-      x: -20,
-      rotate: 0,
-    },
-    hover: {
-      x: 0,
-      rotate: 0,
-    },
-  };
 
-  const largeFirst = {
-    initial: {
-      y: 20,
-      rotate: 0,
-    },
-    hover: {
-      y: 0,
-      rotate: 0,
-    },
-  };
-  const largeSecond = {
-    initial: {
-      y: -20,
-      rotate: 0,
-    },
-    hover: {
-      y: 0,
-      rotate: 0,
-    },
-  };
+  const [display, setDisplay] = useState("scrimba");
+
+  const handleClick = () => {
+    setDisplay(prev => prev === "scrimba" ? "jsMastery": "scrimba");
+  }
+
   return (
     <>
     <motion.div
-      initial="initial"
-      animate="animate"
-      whileHover="hover"
-      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2 2xl:hidden"
+      className="flex flex-col items-center gap-4 flex-1 w-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] space-x-2"
     >
       <motion.div
-        variants={first}
-        className="h-full w-1/2 rounded-2xl bg-white dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-between"
+        className={cn("w-full rounded-2xl bg-white dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-between max-lg:max-w-[400px] max-lg:self-start", display !== "scrimba" && "hidden")}
       >
         <Image
           src="/assets/scrimba-certificate.png"
@@ -219,14 +184,13 @@ const Certificates = () => {
           height={100}
           width={400}
           
-          className="object-contain rounded-tl-2xl rounded-tr-2xl"
+          className=" rounded-tl-2xl rounded-tr-2xl w-full max-lg:max-w-[400px]"
         />
-        <p className="flex-1 flex justify-center items-center">From Scrimba</p>
+        <p className="flex-1 flex justify-center items-center text-[0.9rem] py-2">From Scrimba</p>
       </motion.div>
       
       <motion.div
-        variants={second}
-        className="h-full w-1/2 rounded-2xl bg-white dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-between"
+        className={cn("w-full rounded-2xl bg-white dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-between max-lg:max-w-[500px] max-lg:self-start", display === "scrimba" && "hidden")}
       >
         <Image
           src="/assets/scrimba-certificate.png"
@@ -234,47 +198,27 @@ const Certificates = () => {
           height={100}
           width={400}
           
-          className="object-contain rounded-tl-2xl rounded-tr-2xl"
+          className="object-contain rounded-tl-2xl rounded-tr-2xl w-full max-lg:max-w-[500px]"
         />
-        <p className="flex-1 flex justify-center items-center">From JavaScript Mastery</p>
+        <p className="py-2 flex-1 flex justify-center items-center text-[0.9rem]">From JavaScript Mastery</p>
       </motion.div>
-    </motion.div>
 
-    <motion.div
-      initial="initial"
-      animate="animate"
-      whileHover="hover"
-      className="flex flex-col flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] space-x-2 max-2xl:hidden"
-    >
-      <motion.div
-        variants={largeFirst}
-        className="h-1/2 w-full rounded-2xl bg-white dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-between"
-      >
-        <Image
-          src="/assets/scrimba-certificate.png"
-          alt="avatar"
-          height={100}
-          width={400}
-          
-          className="object-contain rounded-tl-2xl rounded-tr-2xl"
-        />
-        <p className="flex-1 flex justify-center items-center 2xl:text-[1.5rem]">From Scrimba</p>
-      </motion.div>
-      
-      <motion.div
-        variants={largeSecond}
-        className="h-1/2 w-full rounded-2xl bg-white dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-between"
-      >
-        <Image
-          src="/assets/scrimba-certificate.png"
-          alt="avatar"
-          height={100}
-          width={400}
-          
-          className="object-contain rounded-tl-2xl rounded-tr-2xl"
-        />
-        <p className="flex-1 flex justify-center items-center 2xl:text-[1.5rem]">From JavaScript Mastery</p>
-      </motion.div>
+      <div className="max-lg:self-start flex justify-end items-center gap-2 mb-4 pt-2 max-lg:max-w-[500px]">
+        <button
+          className="relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50 cursor-pointer"
+          onClick={handleClick}
+          disabled={display === "scrimba"}
+        >
+          <IconArrowNarrowLeft className="h-6 w-6  text-gray-500" />
+        </button>
+        <button
+          className="relative z-40 h-10 w-10 rounded-full bg-gray-100 flex items-center justify-center disabled:opacity-50 cursor-pointer"
+          onClick={handleClick}
+          disabled={display !== "scrimba"}
+        >
+          <IconArrowNarrowRight className="h-6 w-6 text-gray-500" />
+        </button>
+      </div>
     </motion.div>
     </>
   );
