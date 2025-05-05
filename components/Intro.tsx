4e820de95f0092fd808347edb2b1b96be52190d1
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-05-05 23:12:04 +0200

    Making the intro content absolute.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,29 +2,12 @@
 
 import Link from "next/link";
 import { IconChevronsDown } from "@tabler/icons-react";
-import { useEffect, useState } from "react";
 
 const Intro = () => {
-  
-  const [height, setHeight] = useState(0);
-
-  useEffect(() => {
-    setHeight(window.innerHeight);
-  }, []);
-
-  if (height === 0) return (
-  <section className="relative h-dvh w-full overflow-x-hidden ">
-    <div className="flex items-center justify-center absolute z-[100] h-dvh w-screen overflow-hidden dark:bg-black">
-      <div className="three-body">
-        <div className="three-body__dot"></div>
-        <div className="three-body__dot"></div>
-        <div className="three-body__dot"></div>
-      </div>
-    </div>
-  </section>); // Avoid rendering until height is set
-  
+    
   return (
-    <section className="flex flex-col items-center justify-center overflow-hidden" style={{ height: `${height}px` }}>
+    <section className="relative flex flex-col items-center justify-center overflow-hidden h-dvh">
+      <div className="flex flex-col items-center justify-center absolute z-[100] h-dvh w-screen overflow-hidden dark:bg-black">
         <div className="flex flex-col items-center justify-center font-medium">
           <div className="overflow-hidden w-full">
             <div className="whitespace-nowrap animate-marquee-left w-full flex items-center justify-center">
@@ -57,6 +40,7 @@ const Intro = () => {
             </div>
           </Link>
         </div>
+      </div>
     </section>
   )
 }
