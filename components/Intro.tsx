cf8836921f5fef0c543f688a509c2282918b3850
krishnapriya-venkatakrishnan <krishnapriyavenkatakrishnan@gmail.com>
2025-05-05 23:01:58 +0200

    Implement loading page.

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,10 +2,29 @@
 
 import Link from "next/link";
 import { IconChevronsDown } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 const Intro = () => {
+  
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    setHeight(window.innerHeight);
+  }, []);
+
+  if (height === 0) return (
+  <section className="relative h-dvh w-full overflow-x-hidden ">
+    <div className="flex items-center justify-center absolute z-[100] h-dvh w-screen overflow-hidden dark:bg-black">
+      <div className="three-body">
+        <div className="three-body__dot"></div>
+        <div className="three-body__dot"></div>
+        <div className="three-body__dot"></div>
+      </div>
+    </div>
+  </section>); // Avoid rendering until height is set
+  
   return (
-    <section className="h-[100vh] flex flex-col items-center justify-center overflow-hidden">
+    <section className="flex flex-col items-center justify-center overflow-hidden" style={{ height: `${height}px` }}>
         <div className="flex flex-col items-center justify-center font-medium">
           <div className="overflow-hidden w-full">
             <div className="whitespace-nowrap animate-marquee-left w-full flex items-center justify-center">
